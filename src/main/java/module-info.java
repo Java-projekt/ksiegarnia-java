00774a7af5ee50e6732c04fdd444f290f7f88791
Julia Chmura <chmurajulia0@gmail.com>
2024-06-16 21:19:36 +0200

    zmiany+stage2(not finish)
    zmiany w stagu-zaloguj sie, dodanie klasy book pod tabele w panelu użytkownika, dodanie panelu użytkownika - nie skończona tabela książek

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.ksiegarnia {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
 
     opens org.example.ksiegarnia to javafx.fxml;
---
+++ src/main/resources/org/example/ksiegarnia/panelUzytkownikaController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.example.ksiegarnia;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author chmur
+ */
+public class panelUzytkownikaController implements Initializable {
+
+
+    @FXML
+    private Label lblPanelUzytkownika;
+    @FXML
+    private Label lbTytul;
+    @FXML
+    private Label lblAutor;
+    @FXML
+    private Label lblCena;
+    @FXML
+    private Label lbllosc;
+    @FXML
+    private Label lblDodajKsiazke;
+    @FXML
+    private TextField tfTytul;
+    @FXML
+    private TextField tfAutor;
+    @FXML
+    private TextField tfCena;
+    @FXML
+    private TextField tfIlosc;
+    @FXML
+    private Button btnDodajKsiazke;
+    @FXML
+    private Label lblWyszukaj;
+    @FXML
+    private TextField tfSzukaj;
+    @FXML
+    private Button btnSzukaj;
+    @FXML
+    private Button btnWyloguj;
+    @FXML
+    private TableColumn<Book, String> tabColumnTytul;
+    @FXML
+    private TableColumn<Book, String> tabColumnAutor;
+    @FXML
+    private TableColumn<Book, Double> tabColumnCena;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void onBtnDodajClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onBtnSzukajClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void onBtnWylogujClick(ActionEvent event) {
+    }
+
+}
