8ebf187a36ca7cc6c99058854d00d4c76a136b47
Julia Chmura <chmurajulia0@gmail.com>
2024-06-16 18:04:06 +0200

    stage1

--- src/main/java/org/example/ksiegarnia/HelloController.java
+++ src/main/java/org/example/ksiegarnia/HelloController.java
@@ -1,11 +1,24 @@
 package org.example.ksiegarnia;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
     private Label welcomeText;
+    @FXML
+    private Label lbZaloguj;
+    @FXML
+    private Button btnZaloguj;
+    @FXML
+    private TextField tfHaslo;
+    @FXML
+    private TextField tfLogin;
+    @FXML
+    private Label lbLogin;
+    @FXML
+    private Label lbHaslo;
 
     @FXML
     protected void onHelloButtonClick() {
